4724357a2f8a2dfa72cf3817cccfb1c44674a5c5
Luiz Henrique Gonçalves da Silva <h.m.silva78@gmail.com>
2023-07-15 17:35:14 -0300

    inner class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ fun main() {
     val bluePen = Musician()
     bluePen.desc()
 
-    val guitar = Musician.Guitar()
+    val guitar = bluePen.Guitar()
     guitar.desc()
 }
 
@@ -14,8 +14,8 @@ class Musician {
 
     fun desc() = println("Meu estilo é $style")
 
-    class Guitar() {
+    inner class Guitar() {
         var strings = 6
-        fun desc() = println("Tocando em uma guitarra com $strings cordas")
+        fun desc() = println("Tocando em uma guitarra com $strings cordas, e o estilo é $style")
     }
 }
