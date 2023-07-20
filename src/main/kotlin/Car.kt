d993607bae7960b8cb3c9e4834ea5d9841b63bf4
Luiz Henrique Gonçalves da Silva <h.m.silva78@gmail.com>
2023-07-19 22:12:35 -0300

    Bidirecional abstract class

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,7 +1,13 @@
 abstract class Car(var name: String, var brand: String) {
-    abstract fun power(power: Double)
 
-    fun toDrive() {
+    abstract fun power(): Boolean
+
+    fun toDrive(driver: Driver) {
+        if(power()) {
+            println("Sendo dirigido pelo ${driver.name}")
+        } else {
+            println("Potência não inserida")
+        }
         when(name) {
             "Civic" -> println("VRUMMMMVRUMMMMM")
             "Siena" -> println("PIPIPIPOPOPO")
@@ -12,19 +18,24 @@ abstract class Car(var name: String, var brand: String) {
 }
 
 class Siena(name: String, brand: String) : Car(name, brand) {
-    override fun power(power: Double) {
-        println("O carro $name, da marca $brand e tem $power de potência")
+    override fun power(): Boolean {
+        println("O carro $name é da marca $brand e tem 200 cavalos de potência")
+        return true
     }
 }
 
 class Corolla(name: String, brand: String) : Car(name, brand) {
-    override fun power(power: Double) {
-        println("O carro $name, da marca $brand e tem $power de potência")
+    override fun power(): Boolean {
+        println("O carro $name é da marca $brand e tem 400 cavalos de potência")
+        return false
     }
 }
 
 class Civic(name: String, brand: String) : Car(name, brand) {
-    override fun power(power: Double) {
-        println("O carro $name, da marca $brand e tem $power de potência")
+    override fun power(): Boolean {
+        println("O carro $name é da marca $brand e tem 350 cavalos de potência")
+        return true
     }
 }
+
+class Driver(val name: String)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,14 @@
 fun main() {
 
     val car1 = Siena("Siena", "FIAT")
-    car1.power(1.6)
-    car1.toDrive()
+    car1.power()
+    car1.toDrive(Driver("Marcos"))
 
     val car2 = Corolla("Corolla", "Toyota")
-    car2.power(3.0)
-    car2.toDrive()
+    car2.power()
+    car2.toDrive(Driver("Rogerio"))
 
     val car3 = Civic("Civic", "Honda")
-    car3.power(2.0)
-    car3.toDrive()
+    car3.power()
+    car3.toDrive(Driver("José"))
 }
