914036f2c8c8959b01b7ff2a85d392073d7173a3
Luiz Henrique Gonçalves da Silva <h.m.silva78@gmail.com>
2023-07-20 22:40:58 -0300

    Undertand collections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,41 +1,14 @@
 fun main() {
 
-    val btn = Button("Salvar", 0XFFF0000, Pair(20, 30))
-    btn.render()
+    val items = arrayOf(1, "Leandro", 3, 4, 5.10)
+    items.forEach {
+        println(it)
+    }
 
-    val link = Link("Entrar", Pair(15, 20))
-    link.render()
+    val num = intArrayOf(1, 2, 3, 4, 5)
+    num.forEach { number ->
+        println(number)
+    }
 
 }
 
-abstract class Component {
-    abstract fun position() : Pair<Int, Int>
-
-    open fun render() {
-        println("tela: ${position().first} por ${position().second}")
-    }
-}
-
-abstract class Text(val text: String) : Component() {
-    override fun render() {
-        super.render()
-        println("Desenhando o texto: $text")
-    }
-}
-
-class Button(text: String, val backgroundColor: Int, val pos: Pair<Int, Int>) : Text(text) {
-    override fun position(): Pair<Int, Int> {
-        return pos
-    }
-
-    override fun render() {
-        super.render()
-        println("Pintando o fundo de $backgroundColor")
-    }
-}
-
-class Link(text: String, val pos: Pair<Int, Int>) : Text(text) {
-    override fun position(): Pair<Int, Int> {
-        return pos
-    }
-}
