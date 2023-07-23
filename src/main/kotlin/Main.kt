6fe5f900f4f1016c5c868a2dd95fd22c5a18e861
Luiz Henrique Gonçalves da Silva <h.m.silva78@gmail.com>
2023-07-22 23:01:24 -0300

    list combination

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,18 @@
 fun main() {
 
-    val cars = mutableListOf<Car>(
-        Car("Siena", "Fiat"),
-        Car("Civic", "Honda"),
-        Car("911", "Porsche")
-    )
-    cars.forEach { println(it) }
+    val a = mutableListOf("Jorge", "Maria")
+    val b = listOf("João", "Fadua", "Henrique")
+
+    b.filterTo(a, { it.contains("u") && it.contains("a")})
+    println(a)
 
     println()
 
-    println(cars.firstOrNull())
-
-    println(cars.lastOrNull())
-
-    println()
-
-    println(cars.isEmpty())
-    cars[1] = Car("Renegade", "Jeep")
-    cars.forEach { println(it) }
-
-    println()
-
-    cars.add(Car("Toro", "Fiat"))
-    cars.forEach { println(it) }
-
-    println()
-
-    val emptyList = emptyList<Car?>()
+    val c = listOf("A", "B")
+    val d = listOf("E", "F")
+    val all = listOf(c, d)
+    println(all)
+    println(all.flatten())
 
 }
 
