fe30e7144c2e342acbe3a500e1e4e9392711b11c
Luiz Henrique Gonçalves da Silva <h.m.silva78@gmail.com>
2023-07-25 22:06:36 -0300

    Funcional Program

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,31 @@
 fun main() {
 
-    val cars = setOf(
+    val cars = mutableSetOf(
         "Jorge",
         "Jorge",
         "Matheus",
         "Bruce"
     )
 
-    cars.forEach {
-        println(it) // Só vai imprimir apenas uma vez o conteúdo repetido
-    }
+    calc(6, 7, ::sum)
+    calc(6, 4, ::minus)
+    calc(8, 2, ::divide)
+
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun minus(x: Int, y: Int): Int {
+    return x - y
+}
+
+fun divide(x: Int, y: Int): Int {
+    return x / y
+}
+
+fun calc(a: Int, b: Int, operation: (Int, Int) -> Int) {
+    val result = operation(a, b)
+    println("Resultado da operação $a com $b foi $result")
 }
