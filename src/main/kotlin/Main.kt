b31980d53cbb2f4beab155747e0c1710207d522d
Luiz Henrique Gonçalves da Silva <h.m.silva78@gmail.com>
2023-07-28 19:22:33 -0300

    union

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,8 @@
 fun main() {
 
-    val cars = mutableSetOf(
-        Car("Siena", true),
-        Car("Onix", true),
-        Car("Ford Ka", false)
-    )
+    val numbers = listOf(1, 2, 3, 5)
+    val numbers2 = listOf(1, 2, 4, 8, 16)
+    val result = numbers.union(numbers2)
 
-    /*calc(6, 7, ::sum)
-    calc(6, 4, ::minus)
-    calc(8, 2, ::divide)
-
-    calc(8, 2) { a,b ->
-        a * b
-    }*/
-
-    cars.forEach {
-        println(it)
-    }
-
-    val listNames = cars.map { it.name.uppercase() }
-                        .filter { it.contains("A") }
-
-    listNames.forEach { println(it) }
-}
-
-fun sum(x: Int, y: Int): Int {
-    return x + y
-}
-
-fun minus(x: Int, y: Int): Int {
-    return x - y
-}
-
-fun divide(x: Int, y: Int): Int {
-    return x / y
-}
-
-fun filtro(car: Car): Boolean {
-    return car.name.lowercase().contains("ix")
-}
-
-fun calc(a: Int, b: Int, operation: (Int, Int) -> Int) {
-    val result = operation(a, b)
-    println("Resultado da operação $a com $b foi $result")
+    result.forEach { println(it) }
 }
