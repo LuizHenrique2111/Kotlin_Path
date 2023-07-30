f92925ffd3c1fb7ededa046bf80b79aa7b4c445d
Luiz Henrique Gonçalves da Silva <h.m.silva78@gmail.com>
2023-07-30 20:06:19 -0300

    Any Class 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,12 @@
 fun main() {
 
-//    val verdadeiro : Any = true
-//    val name : Any = "Luiz"
-//    val age : Any = 25
-//
-//    println("$verdadeiro | $name | ${age::class}")
-
     val obj: Any = getObject(2)
-    println(obj)
-    if (obj is String) { // Só consegue manipular os atributos do tipo utilizando verificações
-        println(obj.length)
-    }
+//    if (obj is String) { // Só consegue manipular os atributos do tipo utilizando verificações
+//        println(obj.length)
+//    }
+
+    val cast = obj as String // Consigo manipular fazendo a conversão(casting)
+    println(cast.length)
 
 }
 
