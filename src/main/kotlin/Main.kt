729d3a98efbe63975f38490574db071d9116d08c
Luiz Henrique Gonçalves da Silva <h.m.silva78@gmail.com>
2023-07-30 19:46:10 -0300

    Any class 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,54 +1,21 @@
 fun main() {
 
-    val products = mutableMapOf(
-        "Android" to "Google",
-        "iOS" to "Apple",
-        "Windows" to "Microsoft"
-    )
+//    val verdadeiro : Any = true
+//    val name : Any = "Luiz"
+//    val age : Any = 25
+//
+//    println("$verdadeiro | $name | ${age::class}")
 
-//    val res = products["iOS"]
-//    val res = products.getOrDefault("Android2", "Não existe empresa com este SO")
-//    val res = products.getOrElse("Andoid2") {
-//        println("você envia para o servidor")
-//        "Não existe empresa com este SO"
-//    }
-//    println(res)
+    val obj: Any = getObject(2)
+    println(obj)
 
-//    val hasWindows = products.containsKey("Windows")
-//    if(hasWindows) {
-//        println("Faça algo")
-//    }
+}
 
-//    println(products.keys)
-//    println(products.values)
-//
-//    products["iOS"] = "Amazon"
-//    println()
-//
-//    println(products.keys)
-//    println(products.values)
-//
-//    products.remove("Windows")
-//    println()
-//
-//    println(products.keys)
-//    println(products.values)
-//
-//    products.clear()
-//    println()
-//
-//    println(products.keys)
-//    println(products.values)
-//
-//    if (products.isEmpty()) {
-//        println("Não existem produtos cadastrados.")
-//    }
-
-//    val r = products.filter { it.value.contains("e") }
-//                    .map { it.value.substring(0, 1) }
-//    println(r)
-
-    val cars = Car.createCars(10)
-    println(cars.map { it.brand.uppercase() })
-
+fun getObject(value: Int): Any {
+    return when(value) {
+        1 -> 1
+        2 -> "Class"
+        3 -> true
+        else -> 1.0
+    }
 }
